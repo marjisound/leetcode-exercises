397b3de9bb8bf39df1cc43f17d1ccb316dedd5d1
Marjan K <marjank@30728.gnm.int>
2021-03-23 10:32:49 +0000

    two sum question

--- out/twoSum.js
+++ out/twoSum.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("new file");
+function twoSum(nums, target) {
+    var result = [];
+    var inProgress = true;
+    for (var i = 0; i < nums.length && inProgress; i++) {
+        console.log("new iteration i:", i);
+        for (var j = i + 1; j < nums.length; j++) {
+            var addedNums = nums[i] + nums[j];
+            console.log("i: ", i);
+            console.log("j: ", j);
+            console.log("addedNums: ", addedNums);
+            if (addedNums == target) {
+                result = [i, j];
+                inProgress = false;
+                break;
+            }
+        }
+    }
+    return result;
+}
+twoSum([3, 3, 4], 6);
+//let myArray = [2, 7, 11, 15]; let target = 11;
